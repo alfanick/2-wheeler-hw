b3a5a2997ca574376f1caace89830408f652da6c
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2014-06-19 17:31:10 +0200

    resttructurized code for viisblity

--- platform.h
+++ platform.h
@@ -1,12 +1,6 @@
 #ifndef TWO_WHEELER_PLATFORM_H_
 #define TWO_WHEELER_PLATFORM_H_
 
-#include <platform.h>
-#include <xs1.h>
-
-#include <distance_sensor.h>
-#include <lsm303d.h>
-
 #define   FRONT_DISTANCE_SENSOR_TRIGGER            XS1_PORT_1I
 #define      FRONT_DISTANCE_SENSOR_ECHO            XS1_PORT_1J
 
@@ -18,6 +12,12 @@
 
 #define              BLUETOOTH_TRANSMIT            XS1_PORT_1O
 #define               BLUETOOTH_RECEIVE            XS1_PORT_1P
+
+#include <platform.h>
+#include <xs1.h>
+
+#include <distance_sensor.h>
+#include <lsm303d.h>
 
 distance_sensor_t distance_sensors[2] = {
   { FRONT_DISTANCE_SENSOR_TRIGGER, FRONT_DISTANCE_SENSOR_ECHO },
