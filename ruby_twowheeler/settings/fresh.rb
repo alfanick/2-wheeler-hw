e120706994349aca1056bfea918e8511fa46a411
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2015-04-21 12:12:02 +0200

    saving settings

---
+++ ruby_twowheeler/settings/fresh.rb
@@ -0,0 +1,14 @@
+$balancer.stop!
+
+$balancer.angle_lowpass_delay = 0.004
+$balancer.loop_delay = 0.005
+$balancer.motors_boost = 250
+$balancer.motors_threshold = 0
+$balancer.pid_lowpass = 1.0
+
+$balancer.target = -4.844
+
+$balancer.pid = [5000.0, 7000.0, 30.0]
+
+$balancer.balance!
+
